30771ee2047077ad2732b344ebeca9cea7dc75ac
☺唐鳳☻ <cpan@audreyt.org>
2001-06-09 21:35:44 +0000

    Import of AUTRIJUS/ebx-0.81 from CPAN.
    gitpan-cpan-distribution: ebx
    gitpan-cpan-version:      0.81
    gitpan-cpan-path:         AUTRIJUS/ebx-0.81.tar.gz
    gitpan-cpan-author:       AUTRIJUS
    gitpan-cpan-maturity:     released

--- Sync.pm
+++ Sync.pm
@@ -1,13 +1,58 @@
 package OurNet::BBSApp::Sync;
 require 5.006;
 
-$VERSION = '0.8';
+$VERSION = '0.81';
 
 use strict;
 use integer;
 
 use OurNet::BBS;
 use Mail::Address;
+
+=head1 NAME
+
+OurNet::BBSApp::Sync - Sync between BBS article groups
+
+=head1 SYNOPSIS
+
+    my $sync = OurNet::BBSApp::Sync->new({
+        artgrp     => $local->{boards}{board1}{articles},
+        rartgrp    => $remote->{boards}{board2}{articles},
+        param      => {
+	    lseen   => 0,
+	    rseen   => 0,
+	    remote  => 'bbs.remote.org',
+	    backend => 'BBSAgent',
+	    board   => 'board2',
+	    lmsgid  => '',
+	    msgids  => [
+		'20010610005743.6c+7nbaJ5I63v5Uq3cZxZw@geb.elixus.org',
+		'20010608213307.suqAZQosHH7LxHCXVi1c9A@geb.elixus.org',
+            ]
+        },
+        backend    => 'BBSAgent',
+        logfh      => \*STDOUT,
+    });
+
+    $sync->do_fetch();
+    $sync->do_send();
+
+=head1 DESCRIPTION
+
+L<OurNet::BBSApp::Sync> performs a sophisticated synchronization heuristic
+on two L<OurNet::BBS> ArticleGroup objects. It operates on the first one
+(C<lartgrp>)'s behalf, updates what's being done in the C<param> field, 
+and attempts to determine the minimally needed transactions to run.
+
+The two methods, L<do_fetch> and L<do_send> could be used independently.
+Beyond that, note that the interface might change in the future, and
+currently it's only a complement to the L<ebx> toolkit.
+
+=head1 BUGS
+
+Lots. Please report bugs as much as possible.
+
+=cut
 
 use fields qw/artgrp rartgrp param backend logfh msgidkeep hostname/;
 
@@ -61,8 +106,9 @@ sub retrack {
 
     print $logfh ("  [retrack] #$rseen: checking\n");
 
-    return $rseen 
-	if eval {$rid->[$rseen]{header}{'Message-ID'}} eq $myid->[-1];
+    return $rseen if (eval {
+	$rid->[$rseen]{header}{'Message-ID'}
+    } || '') eq $myid->[-1];
 
     $self->do_retrack(
 	$rid, 
@@ -75,31 +121,27 @@ sub retrack {
 
 sub do_send {
     my $self = $_[0];
-    my $artgrp  = $self->{artgrp};
-    my $rartgrp = $self->{rartgrp};
-    my $param   = $self->{param};
-    my $backend = $self->{backend};
-    my $logfh   = $self->{logfh};
-    my $lseen   = $param->{lseen};
-    return unless $lseen eq int($lseen || 0); # must be int
-
-    print $logfh ("     [send] checking...\n");
-
-    # XXX voodoo witch doctor style preload
-    my $art      = $artgrp->[1] if ($#{@$artgrp});
+    my $artgrp   = $self->{artgrp};
+    my $rartgrp  = $self->{rartgrp};
+    my $param    = $self->{param};
+    my $backend  = $self->{backend};
+    my $logfh    = $self->{logfh};
+    my $lseen    = $param->{lseen};
     my $rbrdname = $param->{board};
 
+    return unless $lseen eq int($lseen || 0); # must be int
     $lseen = $#{@$artgrp} if $#{@$artgrp} < $lseen;
+
+    print $logfh ("     [send] checking...\n");
 
     if ($param->{lmsgid}) { # backtrace
 	++$lseen;
 
 	while (--$lseen > 0) {
-	    $art = eval { $artgrp->[$lseen] } or next;
+	    my $art = eval { $artgrp->[$lseen] } or next;
 
 	    print $logfh ("     [send] #$lseen: backtracing\n");
-	    last unless $art->{header}{From} =~ m/\./ 
-		or $param->{lmsgid} lt $art->{header}{'Message-ID'};
+	    last unless $param->{lmsgid} lt $art->{header}{'Message-ID'};
 	}
 
         $param->{lseen} = $lseen;
@@ -126,13 +168,23 @@ sub do_send {
 
 	my $xorig = $artgrp->board.'.board@'.$self->{hostname};
 
-	if ($backend eq 'NNTP') {
+	if (index(' NNTP MELIX DBI ', $backend) > -1
+	    or ($backend eq 'PlClient' 
+	        and index(' NNTP MELIX DBI ', $rartgrp->backend()) > -1))
+	{
 	    $xart{header}{'X-Originator'} = $xorig;
 	}
-	else {
-	    $xart{body} .= "\n---\nX-Originator: $xorig";
+	elsif (rindex($xart{body}, "--\n¡° Origin:") > -1) {
+	    chomp($xart{body});
+	    chomp($xart{body});
+	    $xart{body} .= "\n¡° X-Originator: $xorig";
 	}
+	else {
+	    $xart{body} .= "--\nX-Originator: $xorig";
+	}
+
 	eval { $rartgrp->{''} = \%xart };
+
 	unless ($@) {
 	    $param->{lseen}  = $lseen;
 	    $param->{lmsgid} = $art->{header}{'Message-ID'};
@@ -167,18 +219,18 @@ sub do_fetch {
 	$rseen	= $param->{rseen};
     }
 
+    return unless defined($rseen) and length($rseen); # requires rseen
+
     $rseen += $last if $rseen < 0;      # negative subscripts
     $rseen = $last  if $rseen > $last;  # upper bound
-
-    return unless defined($rseen) and length($rseen); # requires rseen
 
     print $logfh "    [fetch] #$param->{rseen}: checking\n";
 
     if ($#{$param->{msgids}} >= 0) {
 	$rseen = $self->retrack($rartgrp, $param->{msgids}, $rseen)
-	    if $rseen and eval {
+	    if $rseen and (eval {
 		$rartgrp->[$rseen]{header}{'Message-ID'}
-	    } ne $param->{msgids}[-1];
+	    } || '') ne $param->{msgids}[-1];
     }
     else { # init
 	my $rfirst = (($rseen - $first) > $self->{msgidkeep}) 
@@ -224,7 +276,7 @@ sub do_fetch {
 	my $rhead = $art->{header};
 
 	if (
-	    index($art->{body}, "X-Originator: $xorig") == -1 and
+	    rindex($art->{body}, "X-Originator: $xorig") == -1 and
 	    nth($param->{msgids}, $rhead->{'Message-ID'}) == -1 and
 		($rhead->{'X-Originator'} || '') ne $xorig
 	) {
@@ -242,12 +294,13 @@ sub do_fetch {
 		$xart{body} =~ s|^((?:: )+)|'> ' x (length($1)/2)|meg;
 	    }
 
-            $xart{body}   ||= "\n";
-            $xart{title}  ||= " ";
-            $xart{author} ||= "(nobody)."; # for sanity's sake
+	    # the code below makes us *really* want a ??= operator.
+            $xart{body}   = "\n" unless defined $xart{body};
+            $xart{title}  = " "  unless defined $xart{title};
+            $xart{author} = "(nobody)."
+		unless defined($xart{author}); # for sanity's sake
 
             $artgrp->{''} = \%xart;
-
             print $logfh (" $xart{title}\n");
         }
         else {
@@ -260,7 +313,7 @@ sub do_fetch {
 
     $param->{rseen} = $rseen;
 
-    return 1;
+    return $artgrp->[-1]; # must be here to re-initialize this board
 }
 
 sub safe_copy {
@@ -274,3 +327,25 @@ sub safe_copy {
 }
 
 1;
+
+__END__
+
+=head1 SEE ALSO
+
+L<ebx> -- The Elixir BBS Exchange Suite.
+
+=head1 AUTHORS
+
+Chia-Liang Kao E<lt>clkao@clkao.org>,
+Autrijus Tang E<lt>autrijus@autrijus.org>
+
+=head1 COPYRIGHT
+
+Copyright 2001 by Chia-Liang Kao E<lt>clkao@clkao.org>,
+                  Autrijus Tang E<lt>autrijus@autrijus.org>.
+
+All rights reserved.  You can redistribute and/or modify
+this module under the same terms as Perl itself.
+
+=cut
+
